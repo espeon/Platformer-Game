8fd417fa19173df875d22627557a06b4ecd29687
Kenobi127 <mateolm1207@gmail.com>
2024-02-07 01:26:39 -0600

    set up the player state machine
    not working yet

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,42 +1,42 @@
+class_name Player
 extends CharacterBody2D 
 
-@export var main_speed = 30
-@export var terminal_velocity = 300
-@export var normal_drag = 10
-@export var ice_drag = 5
-@export var gravity = 10
-@export var jump_velocity = 200
-@export var max_jumps = 2
-@export var start_position = Vector2(160, 50)
-@export var max_lives = 5
+@export var main_speed: float = 30
+@export var terminal_velocity: float = 300
+@export var main_drag: float = 10
+@export var gravity: float = 10
+@export var jump_velocity: float = 200
+@export var start_position: Vector2 = Vector2(160, 50)
+@export var max_jumps: int = 2
+@export var max_lives: int = 5
 
-@onready var crouch_shapecast = $CrouchShapeCast2D
-@onready var cshape = $CollisionShape2D
-@onready var sprite = $AnimatedSprite2D
-@onready var anim = $AnimationTree
-@onready var jump_particles = $JumpParticles
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var anim: AnimationTree = $AnimationTree
+@onready var jump_particles: CPUParticles2D = $JumpParticles
+@onready var crouch_shapecast: ShapeCast2D = $CrouchShapeCast2D
+@onready var cshape: CollisionShape2D = $CollisionShape2D
 @onready var standing_cshape = preload("res://assets/collision shapes/player/StandingCollisionShape.tres")
 @onready var crouching_cshape = preload("res://assets/collision shapes/player/CrouchCollisionShape.tres")
 
-var speed = main_speed
-var jump_num = max_jumps
-var lives = max_lives
-var drag = normal_drag
-var is_crouching = false
-var can_move = true
-var stuck_under_object = false
+var speed: float = main_speed
+var drag: float = main_drag
+var jump_num: int = max_jumps
+var lives: int = max_lives
+var is_crouching: bool = false
+var can_move: bool = true
+var stuck_under_object: bool = false
 signal win_signal
 
 
 
 #this happens onece when load into the screen
-func _ready():
+func _ready() -> void:
 	position = start_position
 	anim.active = true
 
 
 #executes every frame, for non physics processes
-func _process(delta):
+func _process(delta) -> void:
 	if position.y > 1000:
 		$FallDeath.play(0.4)
 		lives = max_lives
@@ -48,12 +48,12 @@ func _process(delta):
 		#win()
 	
 
-func hurt():
+func hurt() -> void:
 	can_move = false
 	lives -= 1
 	anim.set("parameters/is_alive/transition_request", "hurt") #hurt
 
-func check_life():
+func check_life() -> void:
 	if lives < 1:
 		die()
 		return
@@ -61,38 +61,32 @@ func check_life():
 	can_move = true
 	anim.set("parameters/is_alive/transition_request", "alive") #hurt
 
-func die():
-	#pass
+func die() -> void:
 	$Death.play()
 	can_move = false
 	anim.set("parameters/is_alive/transition_request", "dead") #die
 	
 	
-func respawn():
+func respawn() -> void:
 	can_move = true
 	lives = max_lives
 	anim.set("parameters/is_alive/transition_request", "alive") #alive
 	position = start_position
 	
 
-func win():
+func win() -> void:
 	$Win.play()
 	emit_signal("win_signal")
 
 #executes every frame dealing with physics processes
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	#falling mechanics
 	if !is_on_floor():
-		drag = normal_drag
 		velocity.y += gravity
 		if velocity.y > terminal_velocity: #implement terminal velocity
 			velocity.y = terminal_velocity
 	else:
 		jump_num = max_jumps #reset number of jumps if on floor	
-		if position.x > 3580: #3580
-			drag = ice_drag
-		else:
-			drag = normal_drag
 	
 	
 	#jumping mechanics
@@ -108,12 +102,9 @@ func _physics_process(delta):
 	
 	#horixontal movement mechanics
 	#horizontal direction is between -1 and 1 both or neither is 0
-	var horizontal_direction = Input.get_action_strength("right") - Input.get_action_strength("left") 
+	var horizontal_direction: float = Input.get_action_strength("right") - Input.get_action_strength("left") 
 	if can_move:
-		if velocity.x*horizontal_direction>terminal_velocity*.75 && drag==ice_drag:
-			velocity.x += terminal_velocity * 0.01 * horizontal_direction
-		else:
-			velocity.x += speed * horizontal_direction #adds the speed times direction to velocity
+		velocity.x += speed * horizontal_direction #adds the speed times direction to velocity
 		velocity.x -= velocity.x * (drag*delta) 
 	else:
 		velocity.x = 0
@@ -151,7 +142,7 @@ func above_head_is_empty() -> bool:
 
 
 #updates the corresponding animation in the AnimateSprite2D
-func update_animations(horizontal_direction):
+func update_animations(horizontal_direction) -> void:
 	if is_on_floor():
 		anim.set("parameters/in_air_state/transition_request", "ground") #ground
 	else:
@@ -176,12 +167,12 @@ func update_animations(horizontal_direction):
 
 
 #flips the animation direction to left or right depending on the direction of movement
-func switch_direction(horizontal_direction): 
+func switch_direction(horizontal_direction) -> void: 
 	sprite.flip_h = (horizontal_direction < 0) #uses horizontal direction to become a boolean
 
 
 #updates the boolean and collision shape when crouching
-func crouch():
+func crouch() -> void:
 	if !is_crouching:
 		is_crouching = true
 		speed = 0 #set velocity to 0
@@ -190,7 +181,7 @@ func crouch():
 
 
 #updates the boolean and collision shape when standing
-func stand():
+func stand() -> void:
 	if is_crouching:
 		is_crouching = false
 		anim.set("crouch_walk_time", 1) #set the animation speed to normal
@@ -199,5 +190,5 @@ func stand():
 		cshape.position.y = -15
 
 
-func _on_skeleton_hurt_player():
+func _on_skeleton_hurt_player() -> void:
 	hurt()
