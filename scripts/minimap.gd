34da1d21ba57e2b63d3811a85c9dc9e417955b73
natalie <emmaischic@gmail.com>
2024-02-15 13:30:59 -0600

    Added base minimap scene

---
+++ scripts/minimap.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+# This is the camera in the subviewport
+@onready var camera = $SubViewportContainer/SubViewport/Camera2D
+# The tilemap that will be viewed in the minimap
+@export var tilemap : TileMap
+# The tileset that will be used in the minimap
+@export var tileset : TileSet
+
+# The node to focus on
+@export var player: Node2D
+
+func _ready():
+	var duped = tilemap.duplicate()
+	$SubViewportContainer/SubViewport.add_child(duped)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	camera.position = player.position
