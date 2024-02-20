05317da17e4f1b2d7752670e595465c383d38064
natalie <emmaischic@gmail.com>
2024-02-19 22:51:28 -0600

    moved tilemap to player instead of current level

--- scripts/minimap.gd
+++ scripts/minimap.gd
@@ -3,12 +3,12 @@ extends CanvasLayer
 # This is the camera in the subviewport
 @onready var camera = $SubViewportContainer/SubViewport/Camera2D
 # TODO: The tilemap that will be viewed in the minimap
-@export var tilemap : TileMap
+@onready var tilemap: TileMap = $"../../TileMap"
 # The tileset that will be used in the minimap
 @export var tileset : TileSet
 
 # The node to focus on
-@export var player: Node2D
+@onready var player: Node2D = $"../../Player"
 
 func _ready():
 	var duped: TileMap = tilemap.duplicate()
