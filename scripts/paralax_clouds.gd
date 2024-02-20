7b2617f0de30784565d4efc7b8d96045b2a6098e
Kenobi127 <mateolm1207@gmail.com>
2024-02-19 23:27:21 -0600

    fixed the clouds movement to be independent from player

--- scripts/paralax_clouds.gd
+++ scripts/paralax_clouds.gd
@@ -1,5 +1,6 @@
 extends ParallaxBackground
 
+# set the base scale to 0 to detatch it from the player movement
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	scroll_offset = Vector2(0, 0)
